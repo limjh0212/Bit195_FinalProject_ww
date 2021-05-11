5a95dd4533962fa4d9614796f4d61cccb5a63826
jh <limjh0210@gmail.com>
2021-05-11 16:22:49 +0900

    project setting.

---
+++ src/main/java/com/bit/ww/WwApplication.java
@@ -0,0 +1,13 @@
+package com.bit.ww;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bit/ww/WwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bit.ww;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
