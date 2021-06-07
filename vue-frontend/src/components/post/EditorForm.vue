016ff5ea9a3a9963b966958519120c100ed7b55e
jh <limjh0210@gmail.com>
2021-06-07 12:19:10 +0900

    Vuetify 적용 에디터 적용(데이터 전송, 이미지 미완성)

--- vue-frontend/src/components/post/EditorForm.vue
+++ vue-frontend/src/components/post/EditorForm.vue
@@ -1,203 +1,92 @@
 <template>
-    <div>
-        <div v-if="editor">
-            <button @click="editor.chain().focus().toggleBold().run()"
-                    :class="{ 'is-active': editor.isActive('bold') }">
-                bold
-            </button>
-            <button @click="editor.chain().focus().toggleItalic().run()"
-                    :class="{ 'is-active': editor.isActive('italic') }">
-                italic
-            </button>
-            <button @click="editor.chain().focus().toggleStrike().run()"
-                    :class="{ 'is-active': editor.isActive('strike') }">
-                strike
-            </button>
-            <button @click="editor.chain().focus().toggleCode().run()"
-                    :class="{ 'is-active': editor.isActive('code') }">
-                code
-            </button>
-            <button @click="editor.chain().focus().unsetAllMarks().run()">
-                clear marks
-            </button>
-            <button @click="editor.chain().focus().clearNodes().run()">
-                clear nodes
-            </button>
-            <button @click="editor.chain().focus().setParagraph().run()"
-                    :class="{ 'is-active': editor.isActive('paragraph') }">
-                paragraph
-            </button>
-            <button @click="editor.chain().focus().toggleHeading({ level: 1 }).run()"
-                    :class="{ 'is-active': editor.isActive('heading', { level: 1 }) }">
-                h1
-            </button>
-            <button @click="editor.chain().focus().toggleHeading({ level: 2 }).run()"
-                    :class="{ 'is-active': editor.isActive('heading', { level: 2 }) }">
-                h2
-            </button>
-            <button @click="editor.chain().focus().toggleHeading({ level: 3 }).run()"
-                    :class="{ 'is-active': editor.isActive('heading', { level: 3 }) }">
-                h3
-            </button>
-            <button @click="editor.chain().focus().toggleHeading({ level: 4 }).run()"
-                    :class="{ 'is-active': editor.isActive('heading', { level: 4 }) }">
-                h4
-            </button>
-            <button @click="editor.chain().focus().toggleHeading({ level: 5 }).run()"
-                    :class="{ 'is-active': editor.isActive('heading', { level: 5 }) }">
-                h5
-            </button>
-            <button @click="editor.chain().focus().toggleHeading({ level: 6 }).run()"
-                    :class="{ 'is-active': editor.isActive('heading', { level: 6 }) }">
-                h6
-            </button>
-            <button @click="editor.chain().focus().toggleBulletList().run()"
-                    :class="{ 'is-active': editor.isActive('bulletList') }">
-                bullet list
-            </button>
-            <button @click="editor.chain().focus().toggleOrderedList().run()"
-                    :class="{ 'is-active': editor.isActive('orderedList') }">
-                ordered list
-            </button>
-            <button @click="editor.chain().focus().toggleCodeBlock().run()"
-                    :class="{ 'is-active': editor.isActive('codeBlock') }">
-                code block
-            </button>
-            <button @click="editor.chain().focus().toggleBlockquote().run()"
-                    :class="{ 'is-active': editor.isActive('blockquote') }">
-                blockquote
-            </button>
-            <button @click="editor.chain().focus().setHorizontalRule().run()">
-                horizontal rule
-            </button>
-            <button @click="editor.chain().focus().setHardBreak().run()">
-                hard break
-            </button>
-            <button @click="editor.chain().focus().undo().run()">
-                undo
-            </button>
-            <button @click="editor.chain().focus().redo().run()">
-                redo
-            </button>
-        </div>
-        <editor-content :editor="editor"/>
-    </div>
+    <el-tiptap :extensions="extensions" :content="content" placeholder="Write something ..."/>
 </template>
 
 <script>
-import {Editor, EditorContent} from '@tiptap/vue-2'
-import StarterKit from '@tiptap/starter-kit'
+import {
+    // all extensions
+    Doc,
+    Text,
+    Paragraph,
+    Heading,
+    Bold,
+    Italic,
+    Strike,
+    Underline,
+    Link,
+    Image,
+    Blockquote,
+    ListItem,
+    BulletList, // use with ListItem
+    OrderedList, // use with ListItem
+    TodoItem,
+    TodoList, // use with TodoItem
+    TextAlign,
+    Indent,
+    HorizontalRule,
+    HardBreak,
+    History,
+    // LineHeight,
+    // Iframe,
+    // CodeBlock,
+    // TrailingNode,
+    // Table, // use with TableHeader, TableCell, TableRow
+    // TableHeader,
+    // TableCell,
+    // TableRow,
+    // FormatClear,
+    // TextColor,
+    // TextHighlight,
+    // Preview,
+    // Print,
+    Fullscreen,
+    CodeView
+    // SelectAll,
+} from "element-tiptap";
+
+import codemirror from "codemirror";
+import "codemirror/lib/codemirror.css"; // import base style
+import "codemirror/mode/xml/xml.js"; // language
+import "codemirror/addon/selection/active-line.js"; // require active-line.js
+import "codemirror/addon/edit/closetag.js"; // autoCloseTags
 
 export default {
-    components: {
-        EditorContent,
-    },
+    name: "Editor",
 
-    data() {
-        return {
-            editor: null,
-        }
-    },
+    data: () => ({
+        extensions: [
+            new Doc(),
+            new Text(),
+            new Paragraph(),
+            new Heading({level: 5}),
+            new Bold({bubble: true}),
+            new Underline({bubble: true}),
+            new Italic({bubble: true}),
+            new Strike({bubble: true}),
+            // new Link({ bubble: true }),
+            new Image(),
+            // new Blockquote(),
+            new TextAlign(),
+            new ListItem(),
+            new BulletList({bubble: true}),
+            new OrderedList({bubble: true}),
+            new TodoItem(),
+            new TodoList(),
+            // new Indent(),
+            new HardBreak(),
+            new HorizontalRule({bubble: true}),
+            // new Fullscreen(),
+            // new CodeView({
+            //     codemirror,
+            //     codemirrorOptions: {
+            //         styleActiveLine: true,
+            //         autoCloseTags: true
+            //     }
+            // }),
+            new History()
+        ],
 
-    mounted() {
-        this.editor = new Editor({
-            extensions: [
-                StarterKit,
-            ],
-            content   : `
-        <h2>
-          Hi there,
-        </h2>
-        <p>
-          this is a <em>basic</em> example of <strong>tiptap</strong>. Sure, there are all kind of basic text styles you’d probably expect from a text editor. But wait until you see the lists:
-        </p>
-        <ul>
-          <li>
-            That’s a bullet list with one …
-          </li>
-          <li>
-            … or two list items.
-          </li>
-        </ul>
-        <p>
-          Isn’t that great? And all of that is editable. But wait, there’s more. Let’s try a code block:
-        </p>
-        <pre><code class="language-css">body {
-  display: none;
-}</code></pre>
-        <p>
-          I know, I know, this is impressive. It’s only the tip of the iceberg though. Give it a try and click a little bit around. Don’t forget to check the other examples too.
-        </p>
-        <blockquote>
-          Wow, that’s amazing. Good work, boy! 👏
-          <br />
-          — Mom
-        </blockquote>
-      `,
-        })
-    },
-
-    beforeDestroy() {
-        this.editor.destroy()
-    },
-}
+        content: ``
+    })
+};
 </script>
-
-<style lang="scss">
-/* Basic editor styles */
-.ProseMirror {
-    > * + * {
-        margin-top: 0.75em;
-    }
-
-    ul,
-    ol {
-        padding: 0 1rem;
-    }
-
-    h1,
-    h2,
-    h3,
-    h4,
-    h5,
-    h6 {
-        line-height: 1.1;
-    }
-
-    code {
-        background-color: rgba(#616161, 0.1);
-        color: #616161;
-    }
-
-    pre {
-        background: #0D0D0D;
-        color: #FFF;
-        font-family: 'JetBrainsMono', monospace;
-        padding: 0.75rem 1rem;
-        border-radius: 0.5rem;
-
-        code {
-            color: inherit;
-            padding: 0;
-            background: none;
-            font-size: 0.8rem;
-        }
-    }
-
-    img {
-        max-width: 100%;
-        height: auto;
-    }
-
-    blockquote {
-        padding-left: 1rem;
-        border-left: 2px solid rgba(#0D0D0D, 0.1);
-    }
-
-    hr {
-        border: none;
-        border-top: 2px solid rgba(#0D0D0D, 0.1);
-        margin: 2rem 0;
-    }
-}
-</style>
