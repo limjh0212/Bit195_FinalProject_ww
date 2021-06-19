bf9b6b4a5f660e6117f09947e1ffc5ab828bf541
jy-agnes-lee <jy.agnes.lee@gmail.com>
2021-06-20 00:04:24 +0900

    img 미리보기 테스트 완료. (수정 진행중)

--- vue-frontend/src/components/post/PostImgForm.vue
+++ vue-frontend/src/components/post/PostImgForm.vue
@@ -1,23 +1,27 @@
 <template>
   <div>
-    <form @submit.prevent="saveimg" method="post" enctype="multipart/form-data">
-      <input type="file" id="File"  name="files" multiple="multiple"></input>
-      <button type="submit" id="Button">저장</button>
-    </form>
+      <div>
+        <form @submit.prevent="saveimg" method="post" enctype="multipart/form-data">
+          <input type="file" id="File"  name="files" multiple="multiple" @change="onFileSelected"></input>
+          <button type="submit" id="Button">저장</button>
+        </form>
+      </div>
+      <div>
+        <img ref="uploadImg" :src="this.imgsrc">
+      </div>
   </div>
 </template>
 
 <script>
 import {saveImg} from "@/api/img";
-
+let FileElement = document.querySelector('#File');
 export default {
   name: "PostImgForm",
   data   : () => ({
-
+      imgsrc : "",
   }),
   methods : {
     async saveimg() {
-      const FileElement = document.querySelector('#File');
       const formData = new FormData();
       // formData.append("boardname", "imgtest")
       // formData.append("uid", this.$store.state.id);
@@ -28,7 +32,18 @@ export default {
       }
       const {data} = await saveImg(formData);
       console.log(data)
-    }
+    },
+    onFileSelected: function (event) {
+      FileElement = event.target;
+      if(FileElement.files[0]) {
+        let img = this.$refs.uploadImg;
+        img.src = URL.createObjectURL(FileElement.files[0]);
+        this.imgsrc = img.src;
+        img.onload = () => {
+          URL.revokeObjectURL(this.src)
+        }
+      }
+    },
   },
 };
 </script>
