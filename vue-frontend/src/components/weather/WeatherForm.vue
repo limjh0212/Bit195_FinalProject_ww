dfd493d32efc4c065dbd99ef56141dcdfc15804d
jy-agnes-lee <jy.agnes.lee@gmail.com>
2021-06-28 12:53:32 +0900

    OOTD 수정, 회원가입 정렬, 관리자페이지 수정 완료.

--- vue-frontend/src/components/weather/WeatherForm.vue
+++ vue-frontend/src/components/weather/WeatherForm.vue
@@ -1,68 +1,67 @@
 <template>
     <div>
-        <div>
-            <h1>오늘의 날씨</h1>
-            <h3>{{ this.locationInfo }}</h3>
-            <li v-for="item in wwResult"><img :src="item" alt="이미지" /></li>
-
-            <table>
-                <thead>
-                    <tr>
-                        <th>날짜</th>
-                        <th>아이콘</th>
-                        <th>날씨</th>
-                        <th>설명</th>
-                        <th>기온</th>
-                        <th>최고기온</th>
-                        <th>최저기온</th>
-                        <th>습도</th>
-                        <th>풍속</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>{{ $moment().format('YYYY-MM-DD') }}</td>
-                        <td>
-                            <v-img :src="this.iconSrc"></v-img>
-                        </td>
-                        <td>{{ this.currentWeather.weather[0].main }}</td>
-                        <td>{{ this.currentWeather.weather[0].description }}</td>
-                        <td>{{ this.currentWeather.temp }}℃</td>
-                        <td>{{ this.weeklyWeather[0].temp.max }}℃</td>
-                        <td>{{ this.weeklyWeather[0].temp.min }}℃</td>
-                        <td>{{ this.currentWeather.humidity }}</td>
-                        <td>{{ this.currentWeather.wind_speed }}</td>
-                    </tr>
-                </tbody>
-            </table>
+      <div id="main-form">
+        <div class="todayWeather box">
+          <div class="card-header card-title">
+            <h4>오늘의 날씨</h4>
+          </div>
+          <div class="card-location">현위치 {{ this.locationInfo }}</div>
+          <div class="card-icon">
+            <img class="weatherIcon" :src="this.iconSrc">
+          </div>
+          <div class="card-weather">
+            <div class="card-weather-box">
+              <div class="card-weather1">{{ this.currentWeather.weather[0].main }}</div>
+              <div class="card-weather3">{{ this.currentWeather.weather[0].description }}</div>
+              <div class="card-weather3">평균 기온  {{ this.currentWeather.temp }}℃</div>
+              <div class="card-weather2">
+                <div class="card-weather2-left"> 최고 기온  {{ this.weeklyWeather[0].temp.max }}℃ </div>
+                <div class="card-weather2-right"> 최저 기온  {{ this.weeklyWeather[0].temp.min }}℃</div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div>
-            <h1>일주일 날씨</h1>
-            <table>
-                <thead>
-                    <tr>
-                        <th>날짜</th>
-                        <th>아이콘</th>
-                        <th>날씨</th>
-                        <th>설명</th>
-                        <th>최고기온</th>
-                        <th>최저기온</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="(item,i) in weeklyWeather" :key="i">
-                        <td>{{ $moment().add(i, 'days').format('YYYY-MM-DD') }}</td>
-                        <td>
-                            <v-img :src="`https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`"></v-img>
-                        </td>
-                        <td>{{ item.weather[0].main }}</td>
-                        <td>{{ item.weather[0].description }}</td>
-                        <td>{{ item.temp.max }}℃</td>
-                        <td>{{ item.temp.min }}℃</td>
-                    </tr>
-                </tbody>
-            </table>
+        <div class="todayWear box">
+          <div class="card-title">
+            <div class="card-header">
+              <h4>오늘의 코디</h4>
+            </div>
+          </div>
+            <div class="card-wear" v-for="item in wwResult">
+              <div class="todayReco"><img class="wear-img" :src="item" alt="이미지" /></div>
+            </div>
         </div>
+        </div>
+<!--        <div>-->
+<!--            <h1>일주일 날씨</h1>-->
+<!--            <table>-->
+<!--                <thead>-->
+<!--                    <tr>-->
+<!--                        <th>날짜</th>-->
+<!--                        <th>아이콘</th>-->
+<!--                        <th>날씨</th>-->
+<!--                        <th>설명</th>-->
+<!--                        <th>최고기온</th>-->
+<!--                        <th>최저기온</th>-->
+<!--                    </tr>-->
+<!--                </thead>-->
+<!--                <tbody>-->
+<!--                    <tr v-for="(item,i) in weeklyWeather" :key="i">-->
+<!--                        <td>{{ $moment().add(i, 'days').format('YYYY-MM-DD') }}</td>-->
+<!--                        <td>-->
+<!--                            <v-img @click="" :src="`https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`"></v-img>-->
+<!--                        </td>-->
+<!--      method(){-->
+<!--      this.dd = item[i].temp.day-->
+<!--      }-->
+<!--                        <td>{{ item.weather[0].main }}</td>-->
+<!--                        <td>{{ item.weather[0].description }}</td>-->
+<!--                        <td>{{ item.temp.max }}℃</td>-->
+<!--                        <td>{{ item.temp.min }}℃</td>-->
+<!--                    </tr>-->
+<!--                </tbody>-->
+<!--            </table>-->
+<!--        </div>-->
         <div v-if="isLoading">
             <LoadingSpinner></LoadingSpinner>
         </div>
