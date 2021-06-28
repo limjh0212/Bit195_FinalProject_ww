a59350aca650f813fa92587e13cfe790a4088fd0
jy-agnes-lee <jy.agnes.lee@gmail.com>
2021-06-28 15:26:57 +0900

    날씨 메인페이지 css 완료.

--- vue-frontend/src/components/weather/WeatherForm.vue
+++ vue-frontend/src/components/weather/WeatherForm.vue
@@ -1,13 +1,13 @@
 <template>
     <div>
       <div id="main-form">
-        <div class="todayWeather box">
+        <div id="todayWeather" class="todayWeather box">
           <div class="card-header card-title">
             <h4>오늘의 날씨</h4>
           </div>
           <div class="card-location">현위치 {{ this.locationInfo }}</div>
           <div class="card-icon">
-            <img class="weatherIcon" :src="this.iconSrc">
+            <div class="card-round"><img class="weatherIcon" :src="this.iconSrc"></div>
           </div>
           <div class="card-weather">
             <div class="card-weather-box">
@@ -21,15 +21,36 @@
             </div>
           </div>
         </div>
-        <div class="todayWear box">
+        <div id="todayWear" class="todayWear box">
           <div class="card-title">
             <div class="card-header">
               <h4>오늘의 코디</h4>
             </div>
           </div>
-            <div class="card-wear" v-for="item in wwResult">
-              <div class="todayReco"><img class="wear-img" :src="item" alt="이미지" /></div>
+          <div class="card-wear" v-for="item in wwResult">
+            <div class="todayReco"><img class="wear-img" :src="item" alt="이미지" /></div>
+          </div>
+        </div>
+        <div id="weeklyWeather" class="weeklyWeather box">
+          <div class="card-title">
+            <div class="card-header">
+              <h4>주간 날씨 정보</h4>
             </div>
+          </div>
+          <div class="card-week">
+            <div class="card-day" v-if="i > 0" v-for="(item,i) in weeklyWeather" :key="i">
+              <div class="card-day-box">{{ $moment().add(i, 'days').format('YYYY-MM-DD') }}</div>
+              <div class="card-day-box">
+                <div><img class="card-day-icon" :src="`https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`"></div>
+              </div>
+              <div class="card-day-box">{{ item.weather[0].main }}</div>
+              <div class="card-day-box">{{ item.temp.day }}℃</div>
+              <div class="card-day-box">
+                <div class="card-day-box-left">{{ item.temp.max }}℃</div>
+                <div class="card-day-box-right">{{ item.temp.min }}℃</div>
+              </div>
+            </div>
+          </div>
         </div>
         </div>
 <!--        <div>-->
@@ -91,7 +112,7 @@ export default {
             this.currentWeather = weatherData.current;
             this.iconSrc = `https://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@2x.png`;
             this.weeklyWeather = weatherData.daily;
-            console.log(weatherData)
+            console.log(this.weatherData);
 
             const Data = {
                 user_id: this.$store.state.id,
