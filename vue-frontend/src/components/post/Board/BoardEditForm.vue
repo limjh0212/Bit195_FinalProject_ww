dd0a147e902d4ed2ac963a8734a3654423497b28
jh <limjh0210@gmail.com>
2021-06-28 18:10:40 +0900

    대댓글 완료

--- vue-frontend/src/components/post/Board/BoardEditForm.vue
+++ vue-frontend/src/components/post/Board/BoardEditForm.vue
@@ -49,8 +49,8 @@
 
         <!--댓글 입력창-->
         <br>
-        <div v-if="showCmnt===true">
-            <div>
+        <div>
+            <div v-if="writeCmnt1Form">
                 <v-textarea v-model="writeCmnt" name="input-7-4" placeholder="댓글을 입력하세요." solo
                             style="width: 700px;"></v-textarea>
                 <div style="display: flex; justify-content: space-between">
@@ -65,7 +65,7 @@
         </div>
 
         <!--댓글 수정-->
-        <div v-if="showCmnt===false">
+        <div v-if="writeCmnt1Edit">
             <v-textarea v-model="editCmnt" name="input-7-4" solo style="width: 700px;"></v-textarea>
             <div style="display: flex; justify-content: space-between">
                 <div>
@@ -76,6 +76,34 @@
                 </div>
             </div>
         </div>
+
+        <!--대댓글 작성-->
+        <div v-if="writeCmnt2Form">
+            <v-textarea v-model="writeCmnt2" name="input-7-4" placeholder="대댓글을 입력하세요." solo
+                        style="width: 700px;"></v-textarea>
+            <div style="display: flex; justify-content: space-between">
+                <div>
+                </div>
+                <div style="width: 50%;">
+                    <button @click="saveCmnt2">저장<i class="material-icons"
+                                                    style="font-size: 18px;">save</i></button>
+                </div>
+            </div>
+        </div>
+
+        <!--대댓글 수정-->
+        <div v-if="writeCmnt2Edit">
+            <v-textarea v-model="editCmnt2" name="input-7-4" solo style="width: 700px;"></v-textarea>
+            <div style="display: flex; justify-content: space-between">
+                <div>
+                </div>
+                <div style="width: 50%;">
+                    <button @click="editSaveCmnt2">저장<i class="material-icons"
+                                                        style="font-size: 18px;">save</i></button>
+                </div>
+            </div>
+        </div>
+
         <br><br>
         <!--댓글-->
         <div style="width: 80%;">
@@ -114,25 +142,13 @@
                     <td v-else style="width: 150px; text-align: center">{{ $moment(item.regdate).format('YYYY-MM-DD') }}
                     </td>
                     <td style="width: 100px; text-align: center">
-                        <span v-if="isWriter(item.writer)" @click=""><i class="material-icons"
-                                                                                     style="font-size: 18px;">edit</i></span>&nbsp;&nbsp;&nbsp;
-                        <span v-if="isWriter(item.writer)" @click=""><i class="material-icons"
-                                                                                      style="font-size: 18px;">clear</i></span>&nbsp;&nbsp;&nbsp;
+                        <span v-if="isWriter(item.writer)" @click="editCmntdo2(idx,i)"><i class="material-icons"
+                                                                                          style="font-size: 18px;">edit</i></span>&nbsp;&nbsp;&nbsp;
+                        <span v-if="isWriter(item.writer)" @click="deleteCmnt2(idx,i)"><i class="material-icons"
+                                                                                          style="font-size: 18px;">clear</i></span>&nbsp;&nbsp;&nbsp;
                     </td>
-                    <!--댓글 수정-->
                 </tr>
-
             </table>
-
-
-            <!--대댓글 작성-->
-            <div>
-                <div>
-                    <textarea v-model="writeCmnt2" placeholder="대댓글 내용을 입력하세요." type="textarea"/>
-                    <button @click="saveCmnt2">저장</button>
-                </div>
-            </div>
-
         </div>
     </div>
 </template>
@@ -194,13 +210,17 @@ export default {
             isEdit: false,
 
             //댓글, 대댓글 작성
-            cmnt1Array: [],
-            cmnt2Array: [],
-            writeCmnt : '',
-            writeCmnt2: '',
-            editCmnt  : '',
-            cmntnum   : '',
-            showCmnt  : false,
+            writeCmnt     : '',
+            writeCmnt2    : '',
+            editCmnt      : '',
+            editCmnt2     : '',
+            cmntnum       : '',
+            cmntnum2      : '',
+            writeCmnt1Form: true,
+            writeCmnt1Edit: false,
+            writeCmnt2Form: false,
+            writeCmnt2Edit: false,
+
         }
     },
     methods: {
@@ -224,6 +244,12 @@ export default {
                 await this.createContent();
             }
         },
+        //대댓글 삭제
+        async deleteCmnt2(idx, i) {
+            this.cmntnum = this.cmnt2[idx][i].num;
+            await api_deleteCmnt1(this.cmntnum);
+            await this.createContent();
+        },
 
         //댓글 수정 실행
         async editCmnt1() {
@@ -235,15 +261,48 @@ export default {
                 writer  : this.$store.state.id
             }
             await api_editCmnt1(this.cmntnum, Data)
-            await this.createContent()
-            this.showCmnt = true;
+            await this.createContent();
+            this.editCmnt = '';
+        },
+        //대댓글 수정 실행
+        async editSaveCmnt2() {
+            const Data = {
+                boardnum: this.post.boardnum,
+                content : this.editCmnt2,
+                cmntnum : this.cmntnum2,
+                postnum : this.$route.params.postnum,
+                writer  : this.$store.state.id,
+                depth   : 1,
+                num     : this.cmntnum
+            }
+            await api_editCmnt1(this.cmntnum, Data)
+            await this.createContent();
+            this.editCmnt = '';
+            this.writeCmnt1Form = true;
+            this.writeCmnt1Edit = false;
+            this.writeCmnt2Form = false;
+            this.writeCmnt2Edit = false;
         },
 
-        //댓글 수정 버튼 작동 (Text area 보여주기)
+        //댓글 수정 버튼
         editCmntdo(idx) {
-            this.showCmnt = false;
             this.cmntnum = this.cmnt[idx].num;
-            this.editCmnt = this.cmnt[idx].content
+            this.editCmnt = this.cmnt[idx].content;
+            this.writeCmnt1Form = false;
+            this.writeCmnt1Edit = true;
+            this.writeCmnt2Form = false;
+            this.writeCmnt2Edit = false;
+        },
+
+        //대댓글 수정 버튼
+        editCmntdo2(idx, i) {
+            this.cmntnum = this.cmnt2[idx][i].num;
+            this.cmntnum2 = this.cmnt2[idx][i].cmntnum;
+            this.editCmnt2 = this.cmnt2[idx][i].content;
+            this.writeCmnt1Form = false;
+            this.writeCmnt1Edit = false;
+            this.writeCmnt2Form = false;
+            this.writeCmnt2Edit = true;
         },
 
         //대댓글 저장
@@ -256,26 +315,17 @@ export default {
             }
             await api_saveCmnt2(this.cmntnum, Data)
             this.writeCmnt2 = '';
-            this.cmnt2Array.pop(i);
             await this.createContent();
         },
 
-        //대댓글 작성폼 선택
-        pushCmnt2(idx) {
-            if (this.cmnt2Array.includes(idx)) {
-                console.log(idx)
-                this.cmnt2Array.pop(idx);
-            } else {
-                this.cmnt1Array = [];
-                this.cmnt2Array = [];
-                this.cmnt2Array.push(idx);
-                this.cmntnum = this.cmnt[idx].num;
-            }
-        },
 
-        //대댓글 작성폼 보여주기
-        showCmnt2(i) {
-            return this.cmnt2Array.includes(i);
+        //대댓글 작성 버튼
+        pushCmnt2(idx) {
+            this.cmntnum = this.cmnt[idx].num;
+            this.writeCmnt1Form = false;
+            this.writeCmnt1Edit = false;
+            this.writeCmnt2Form = true;
+            this.writeCmnt2Edit = false;
         },
 
         //수정모드
